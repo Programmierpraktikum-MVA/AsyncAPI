01fa3ecacc37ccf3eb580a0a4357687384a5cb08
Felix <53702818+f-eliks@users.noreply.github.com>
2023-05-15 17:00:25 +0200

    created rought starting point and structure which can be built on (#17)
    * created rought starting point and structure which can be built on

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod generator;
+mod parser;
+use askama::Template;
+use generator::{cargo_add, cargo_fmt, cargo_init_project, write_to_file, PublishTemplate};
+use parser::parse_asyncapi_yaml_file;
+use std::{fs::create_dir_all, path::Path};
+
 fn main() {
-    println!("Hello, rust async api!");
+    let specfile_path = Path::new("./specs/basic.yaml");
+    let spec = parse_asyncapi_yaml_file(specfile_path).unwrap();
+
+    let title = Path::new(&spec.info.title);
+    let output_path = &Path::new("./output/").join(title);
+
+    let subscribe_channels = spec.get_subscribe_channels();
+    let publish_channels = spec.get_publish_channels();
+
+    // generate
+    let publish = PublishTemplate {
+        server: spec.servers.iter().next().unwrap().1,
+        publish_data: "asdf",
+        subscribe_channels: &subscribe_channels,
+        publish_channels: &publish_channels,
+    };
+    let render = publish.render().unwrap();
+
+    create_dir_all(output_path.join("src")).unwrap();
+    write_to_file(&render, &output_path.join("src/main.rs")).unwrap();
+
+    cargo_init_project(output_path);
+    cargo_fmt(&output_path.join("src/main.rs"));
+    cargo_add(output_path, "tokio"); // when there are more crates move to generator.rs
 }
