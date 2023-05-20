1ad8cf05559042b6c5a67cf058d224d4915a42f7
Felix <f.dumbeck@campus.tu-berlin.de>
2023-05-20 15:32:57 +0200

    removed white spaces

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,6 @@ use std::{
 };
 
 fn main() {
-
-
-
     let specfile_path = Path::new("./example/specs/userSignupSubscriber.yaml");
     let spec = parser::parse_asyncapi_yaml_file(specfile_path).unwrap();
 
