cfad1982da0da2ffd01dedbee07d4dc126e8f883
Stanislav Kosorin <stanislav.kosorin@cresta.ai>
2023-05-21 13:46:16 +0200

    Working references to non-nested messages

--- src/parser/pubsub.rs
+++ src/parser/pubsub.rs
@@ -1,6 +1,6 @@
 use super::schema_parser_mapper;
 use crate::{
-    asyncapi_model::{AsyncAPI, Operation, OperationMessageType, Payload, ReferenceOr, Schema},
+    asyncapi_model::{AsyncAPI, OperationMessageType, Payload, ReferenceOr, Schema},
     parser::common::convert_string_to_valid_type_name,
     template_model::PubsubTemplate,
 };
@@ -16,13 +16,19 @@ fn transform_schema_to_string_vec(schema: &Schema, root_struct_name: &str) -> Ve
         .join("\n")]
 }
 
-fn extract_schemas_from_channels(
-    pub_channels: Vec<(&String, &Operation)>,
-    sub_channels: Vec<(&String, &Operation)>,
-) -> Vec<String> {
-    return pub_channels
+fn parse_message_reference_path(reference: &str) -> Option<String> {
+    if reference.starts_with("#/components/messages") {
+        let path: Vec<&str> = reference.split('/').collect();
+        // skip first two elements ("#" and "components") and return the rest
+        Some(path[3].to_string())
+    } else {
+        None
+    }
+}
+fn extract_schemas_from_asyncapi(spec: &AsyncAPI) -> Vec<String> {
+    let channels_ops = spec.get_all_channels_operations();
+    return channels_ops
         .iter()
-        .chain(sub_channels.iter())
         .flat_map(|x| {
             let root_msg_name = convert_string_to_valid_type_name(x.0, "");
             let channel = x.1;
@@ -38,32 +44,42 @@ fn extract_schemas_from_channels(
                                 transform_schema_to_string_vec(schema, &root_msg_name)
                             }
                             Some(Payload::Any(val)) => {
-                                println!("\nPayload::Any: {:?}", val);
+                                println!("\nWARNING: Unsupported payload (Any): {:?}", val);
                                 vec![]
                             }
                             None => {
-                                println!("\nPayload::None");
+                                println!("\nWARNING: No payload found!");
                                 vec![]
                             }
                         },
-                        ReferenceOr::Reference { reference: _ } => {
-                            println!("\nReferenceOr::Reference");
-                            vec![]
+                        ReferenceOr::Reference { reference } => {
+                            println!("\nReferenceOr::Reference: {:?}", reference);
+                            let schema = spec.get_schema_from_reference(
+                                parse_message_reference_path(reference).unwrap().as_str(),
+                            );
+                            transform_schema_to_string_vec(schema, &root_msg_name)
                         }
                     })
                     .collect(),
-                OperationMessageType::Single(message_ref_or_item) => {
-                    match message_ref_or_item {
-                        ReferenceOr::Item(message) => match &message.payload {
-                            Some(Payload::Schema(schema)) => {
-                                println!("\nsingle schema: {:?}", schema);
-                                transform_schema_to_string_vec(schema, &root_msg_name)
-                            }
-                            _ => vec![], // or handle Payload::Any here
-                        },
-                        ReferenceOr::Reference { reference: _ } => vec![], // or handle ReferenceOr::Reference here
+                OperationMessageType::Single(message_ref_or_item) => match message_ref_or_item {
+                    ReferenceOr::Item(message) => match &message.payload {
+                        Some(Payload::Schema(schema)) => {
+                            println!("\nsingle schema: {:?}", schema);
+                            transform_schema_to_string_vec(schema, &root_msg_name)
+                        }
+                        _val => {
+                            println!("\nWARNING: Unsupported payload (Any): {:?}", _val);
+                            vec![]
+                        }
+                    },
+                    ReferenceOr::Reference { reference } => {
+                        println!("\nReferenceOr::Reference: {:?}", reference);
+                        let schema = spec.get_schema_from_reference(
+                            parse_message_reference_path(reference).unwrap().as_str(),
+                        );
+                        transform_schema_to_string_vec(schema, &root_msg_name)
                     }
-                }
+                },
             }
         })
         .collect();
@@ -78,10 +94,7 @@ pub fn spec_to_pubsub_template_type(spec: AsyncAPI) -> Result<PubsubTemplate, io
     .unwrap()
     .clone();
 
-    let pub_channels = spec.get_publish_channels();
-    let sub_channels = spec.get_subscribe_channels();
-
-    let schemas: Vec<String> = extract_schemas_from_channels(pub_channels, sub_channels);
+    let schemas: Vec<String> = extract_schemas_from_asyncapi(&spec);
     let joined_schemas = schemas.join("\n");
     println!("\nJoined schemas: {:?}", joined_schemas);
 
