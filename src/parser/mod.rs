e719cb098bfc7b6467d42a9685b73817bf9d50be
Stanislav Kosorin <stanislav.kosorin@cresta.ai>
2023-06-12 10:45:00 +0200

    fix fmt

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,8 +1,8 @@
 mod common;
+mod parse_template_context;
 mod preprocessor;
 mod schema_parser;
-mod parse_template_context;
 mod validator;
 pub use common::{parse_spec_to_model, validate_identifier_string};
-pub use schema_parser::schema_parser_mapper;
 pub use parse_template_context::spec_to_pubsub_template_type;
+pub use schema_parser::schema_parser_mapper;
