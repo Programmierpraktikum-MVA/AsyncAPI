eef629f333d32ea9c2b18ca4b54a6b78facfe2b4
Aro <rother@mail.tu-berlin.de>
2023-06-16 08:52:02 +0200

    made rustdoc generation opt-in

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -8,16 +8,24 @@ pub struct Args {
     // Add a positional argument that the user has to supply:
     /// The specification file to parse      
     #[arg(short, long, default_value = "./example/specs/basic.yaml")]
-    pub specification_file: String,
+    pub specification: String,
 
     /// The output directory
     /// If not specified, the output will be written to ./output/
     /// If the directory does not exist, it will be created
     #[arg(short, long, default_value = "./output/")]
-    pub output_directory: String,
+    pub output: String,
 
     /// The name of the project
     /// If not specified, the name will be taken from the specification file
     #[arg(short, long)]
-    pub project_title: Option<String>,
+    pub title: Option<String>,
+
+    ///Optionally generate Rustdoc for generated Microservice
+    #[arg(short, long, default_value = "false")]
+    pub doc: bool,
+
+    ///Optionally enable logging in generated Microservice
+    #[arg(short, long, default_value = "false")]
+    pub log: bool,
 }
