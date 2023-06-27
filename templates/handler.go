21b6561e9c855bbf284b8a2ba6e98dd3686e1a21
Jacob Große <jacobgrosse@Jacobs-Spectre.fritz.box>
2023-06-27 23:16:35 +0200

    expanded templates to support one stream, test with test_stream.yml, for basic.yaml fails at common 55 while injecting

--- templates/handler.go
+++ templates/handler.go
@@ -1,5 +1,11 @@
-use async_nats::{Client, Message};
+use async_nats::{Client, Message, jetstream};
+use async_nats::jetstream::Context;
 use crate::{publish_message,
+{{ range .subscribe_channels }}
+{{ if (index . 1).original_operation.bindings.nats.streamname }}
+    stream_publish_message,
+{{end}}
+{{end}}
 {{ range .model.messages }}
     {{ if .payload }}
         model::{{ .payload.unique_id }},
@@ -21,6 +27,49 @@ use std::time;
     /// {{ range (index . 1).messages }}
     ///     {{ .unique_id }}
     /// {{ end }}
+    {{ if (index . 1).original_operation.bindings.nats.streamname }}
+    pub fn stream_handler_{{ (index . 1).unique_id }}(message: jetstream::Message) {
+        {{ if (index . 1).multiple_messages_enum }}
+            match serde_json::from_slice::<{{ (index . 1).multiple_messages_enum.unique_id }}>(&message.message.payload.as_ref()) {
+                Ok(deserialized_message) => {
+                    // TODO: Replace this with your own handler code
+                    println!("Received message {:#?}", deserialized_message);
+                },
+                Err(_) => {
+                    println!("Failed to deserialize message payload: {{ (index . 1).multiple_messages_enum.unique_id }}\nOriginal message: {:#?}", message);
+                    // TODO: Handle the failed deserialization here
+                },
+            }
+        {{else}}
+        {{ range (index . 1).messages }}
+        match serde_json::from_slice::<{{ .unique_id }}>(&message.message.payload.as_ref()) {
+            Ok(deserialized_message) => {
+                println!("Received message {:#?}", deserialized_message);
+                // TODO: Replace this with your own handler code
+                {{ if .payload}}
+                {{ if .payload.multiple_payload_enum}}
+                    // TODO: this is always None for now (unreachable),
+                    // take a look the comment in src/template_model/simplified_operation.rs/simplify_schema
+                    match deserialized_message.payload {
+                        {{$enumName := .payload.multiple_payload_enum.unique_id}}
+                        {{ range .payload.multiple_payload_enum.messages }}
+                            {{ $enumName }}::{{ .unique_id }}(payload) => {
+                            println!("Received message payload {{ .unique_id }}", payload);
+                            }   
+                        {{ end }}
+                    }
+                {{ end }}
+                {{ end }}
+            },
+            Err(_) => {
+                println!("Failed to deserialize message payload: {{ .unique_id }}\nOriginal message: {:#?}", message);
+                // TODO: Handle the failed deserialization here
+            },
+        }
+        {{ end }}
+        {{ end }}
+    }
+    {{else}}
     pub fn handler_{{ (index . 1).unique_id }}(message: Message) {
         {{ if (index . 1).multiple_messages_enum }}
             match serde_json::from_slice::<{{ (index . 1).multiple_messages_enum.unique_id }}>(&message.payload.as_ref()) {
@@ -62,6 +111,7 @@ use std::time;
         {{ end }}
         {{ end }}
     }
+    {{end}}
 {{ end  }}
 
 {{ range .subscribe_channels }}
@@ -70,6 +120,16 @@ use std::time;
     /// {{ range (index . 1).messages }}
     ///     {{ .unique_id }}
     /// {{ end }}
+    {{ if (index . 1).original_operation.bindings.nats.streamname }}
+    pub async fn stream_producer_{{ (index . 1).unique_id }}(context_stream: &Context, channel: &str) { //context instead of client
+        // This is just an example producer, publishing a message every 2 seconds
+        // TODO: replace this with your own producer code
+        loop {
+            tokio::time::sleep(time::Duration::from_secs(2)).await;
+            stream_publish_message(context_stream, channel, "{\"test\":\"serialized\"}").await;
+        }
+    }
+    {{else}}
     pub async fn producer_{{ (index . 1).unique_id }}(client: &Client, channel: &str) {
         // This is just an example producer, publishing a message every 2 seconds
         // TODO: replace this with your own producer code
@@ -78,6 +138,7 @@ use std::time;
             publish_message(client, channel, "{\"test\":\"serialized\"}").await;
         }
     }
+    {{end}}
 {{ end  }}
 
 
