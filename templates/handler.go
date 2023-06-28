be941c436fb98cdf803b11cb78426964e5ab3672
Jacob Große <jacobgrosse@Jacobs-Spectre.fritz.box>
2023-06-28 22:23:46 +0200

    templates works now, also made them look nicer

--- templates/handler.go
+++ templates/handler.go
@@ -30,49 +30,93 @@ use std::time;
     ///     {{ .unique_id }}
     /// {{ end }}
     {{ if (index . 1).original_operation.bindings }}
-    {{ if (index . 1).original_operation.bindings.nats.streamname }}
-    pub fn stream_handler_{{ (index . 1).unique_id }}(message: jetstream::Message) {
-        {{ if (index . 1).multiple_messages_enum }}
-            match serde_json::from_slice::<{{ (index . 1).multiple_messages_enum.unique_id }}>(&message.message.payload.as_ref()) {
-                Ok(deserialized_message) => {
-                    // TODO: Replace this with your own handler code
-                    println!("Received message {:#?}", deserialized_message);
-                },
-                Err(_) => {
-                    println!("Failed to deserialize message payload: {{ (index . 1).multiple_messages_enum.unique_id }}\nOriginal message: {:#?}", message);
-                    // TODO: Handle the failed deserialization here
-                },
-            }
-        {{else}}
-        {{ range (index . 1).messages }}
-        match serde_json::from_slice::<{{ .unique_id }}>(&message.message.payload.as_ref()) {
-            Ok(deserialized_message) => {
-                println!("Received message {:#?}", deserialized_message);
-                // TODO: Replace this with your own handler code
-                {{ if .payload}}
-                {{ if .payload.multiple_payload_enum}}
-                    // TODO: this is always None for now (unreachable),
-                    // take a look the comment in src/template_model/simplified_operation.rs/simplify_schema
-                    match deserialized_message.payload {
-                        {{$enumName := .payload.multiple_payload_enum.unique_id}}
-                        {{ range .payload.multiple_payload_enum.messages }}
-                            {{ $enumName }}::{{ .unique_id }}(payload) => {
-                            println!("Received message payload {{ .unique_id }}", payload);
-                            }   
-                        {{ end }}
+        {{ if (index . 1).original_operation.bindings.nats.streamname }}
+            pub fn stream_handler_{{ (index . 1).unique_id }}(message: jetstream::Message) {
+                {{ if (index . 1).multiple_messages_enum }}
+                    match serde_json::from_slice::<{{ (index . 1).multiple_messages_enum.unique_id }}>(&message.message.payload.as_ref()) {
+                        Ok(deserialized_message) => {
+                            // TODO: Replace this with your own handler code
+                            println!("Received message {:#?}", deserialized_message);
+                        },
+                        Err(_) => {
+                            println!("Failed to deserialize message payload: {{ (index . 1).multiple_messages_enum.unique_id }}\nOriginal message: {:#?}", message);
+                            // TODO: Handle the failed deserialization here
+                        },
                     }
+                {{else}}
+                    {{ range (index . 1).messages }}
+                        match serde_json::from_slice::<{{ .unique_id }}>(&message.message.payload.as_ref()) {
+                            Ok(deserialized_message) => {
+                                println!("Received message {:#?}", deserialized_message);
+                                // TODO: Replace this with your own handler code
+                                {{ if .payload}}
+                                    {{ if .payload.multiple_payload_enum}}
+                                        // TODO: this is always None for now (unreachable),
+                                        // take a look the comment in src/template_model/simplified_operation.rs/simplify_schema
+                                        match deserialized_message.payload {
+                                            {{$enumName := .payload.multiple_payload_enum.unique_id}}
+                                            {{ range .payload.multiple_payload_enum.messages }}
+                                                {{ $enumName }}::{{ .unique_id }}(payload) => {
+                                                println!("Received message payload {{ .unique_id }}", payload);
+                                                }   
+                                            {{ end }}
+                                        }
+                                    {{ end }}
+                                {{ end }}
+                            },
+                            Err(_) => {
+                                println!("Failed to deserialize message payload: {{ .unique_id }}\nOriginal message: {:#?}", message);
+                                // TODO: Handle the failed deserialization here
+                            },
+                        }
+                    {{ end }}
                 {{ end }}
+            }
+        
+    
+        {{else}}
+            pub fn handler_{{ (index . 1).unique_id }}(message: Message) {
+                {{ if (index . 1).multiple_messages_enum }}
+                    match serde_json::from_slice::<{{ (index . 1).multiple_messages_enum.unique_id }}>(&message.payload.as_ref()) {
+                        Ok(deserialized_message) => {
+                            // TODO: Replace this with your own handler code
+                            println!("Received message {:#?}", deserialized_message);
+                        },
+                        Err(_) => {
+                            println!("Failed to deserialize message payload: {{ (index . 1).multiple_messages_enum.unique_id }}\nOriginal message: {:#?}", message);
+                            // TODO: Handle the failed deserialization here
+                        },
+                    }
+                {{else}}
+                    {{ range (index . 1).messages }}
+                        match serde_json::from_slice::<{{ .unique_id }}>(&message.payload.as_ref()) {
+                            Ok(deserialized_message) => {
+                                println!("Received message {:#?}", deserialized_message);
+                                // TODO: Replace this with your own handler code
+                                {{ if .payload}}
+                                    {{ if .payload.multiple_payload_enum}}
+                                        // TODO: this is always None for now (unreachable),
+                                        // take a look the comment in src/template_model/simplified_operation.rs/simplify_schema
+                                        match deserialized_message.payload {
+                                            {{$enumName := .payload.multiple_payload_enum.unique_id}}
+                                            {{ range .payload.multiple_payload_enum.messages }}
+                                                {{ $enumName }}::{{ .unique_id }}(payload) => {
+                                                println!("Received message payload {{ .unique_id }}", payload);
+                                                }   
+                                            {{ end }}
+                                        }
+                                    {{ end }}
+                                {{ end }}
+                            },
+                            Err(_) => {
+                                println!("Failed to deserialize message payload: {{ .unique_id }}\nOriginal message: {:#?}", message);
+                                // TODO: Handle the failed deserialization here
+                            },
+                        }
+                    {{ end }}
                 {{ end }}
-            },
-            Err(_) => {
-                println!("Failed to deserialize message payload: {{ .unique_id }}\nOriginal message: {:#?}", message);
-                // TODO: Handle the failed deserialization here
-            },
-        }
-        {{ end }}
-        {{ end }}
-        {{ end }}
-    }
+            }
+        {{end}}
     {{else}}
     pub fn handler_{{ (index . 1).unique_id }}(message: Message) {
         {{ if (index . 1).multiple_messages_enum }}
@@ -87,35 +131,36 @@ use std::time;
                 },
             }
         {{else}}
-        {{ range (index . 1).messages }}
-        match serde_json::from_slice::<{{ .unique_id }}>(&message.payload.as_ref()) {
-            Ok(deserialized_message) => {
-                println!("Received message {:#?}", deserialized_message);
-                // TODO: Replace this with your own handler code
-                {{ if .payload}}
-                {{ if .payload.multiple_payload_enum}}
-                    // TODO: this is always None for now (unreachable),
-                    // take a look the comment in src/template_model/simplified_operation.rs/simplify_schema
-                    match deserialized_message.payload {
-                        {{$enumName := .payload.multiple_payload_enum.unique_id}}
-                        {{ range .payload.multiple_payload_enum.messages }}
-                            {{ $enumName }}::{{ .unique_id }}(payload) => {
-                            println!("Received message payload {{ .unique_id }}", payload);
-                            }   
+            {{ range (index . 1).messages }}
+                match serde_json::from_slice::<{{ .unique_id }}>(&message.payload.as_ref()) {
+                    Ok(deserialized_message) => {
+                        println!("Received message {:#?}", deserialized_message);
+                        // TODO: Replace this with your own handler code
+                        {{ if .payload}}
+                            {{ if .payload.multiple_payload_enum}}
+                                // TODO: this is always None for now (unreachable),
+                                // take a look the comment in src/template_model/simplified_operation.rs/simplify_schema
+                                match deserialized_message.payload {
+                                    {{$enumName := .payload.multiple_payload_enum.unique_id}}
+                                    {{ range .payload.multiple_payload_enum.messages }}
+                                        {{ $enumName }}::{{ .unique_id }}(payload) => {
+                                        println!("Received message payload {{ .unique_id }}", payload);
+                                        }   
+                                    {{ end }}
+                                }
+                            {{ end }}
                         {{ end }}
-                    }
-                {{ end }}
-                {{ end }}
-            },
-            Err(_) => {
-                println!("Failed to deserialize message payload: {{ .unique_id }}\nOriginal message: {:#?}", message);
-                // TODO: Handle the failed deserialization here
-            },
-        }
-        {{ end }}
+                    },
+                    Err(_) => {
+                        println!("Failed to deserialize message payload: {{ .unique_id }}\nOriginal message: {:#?}", message);
+                        // TODO: Handle the failed deserialization here
+                    },
+                }
+            {{ end }}
         {{ end }}
     }
-    {{end}}
+    {{ end }}
+
 {{ end  }}
 
 {{ range .subscribe_channels }}
@@ -125,26 +170,35 @@ use std::time;
     ///     {{ .unique_id }}
     /// {{ end }}
     {{ if (index . 1).original_operation.bindings }}
-    {{ if (index . 1).original_operation.bindings.nats.streamname }}
-    pub async fn stream_producer_{{ (index . 1).unique_id }}(context_stream: &Context, channel: &str) { //context instead of client
-        // This is just an example producer, publishing a message every 2 seconds
-        // TODO: replace this with your own producer code
-        loop {
-            tokio::time::sleep(time::Duration::from_secs(2)).await;
-            stream_publish_message(context_stream, channel, "{\"test\":\"serialized\"}").await;
+        {{ if (index . 1).original_operation.bindings.nats.streamname }}
+            pub async fn stream_producer_{{ (index . 1).unique_id }}(context_stream: &Context, channel: &str) { //context instead of client
+                // This is just an example producer, publishing a message every 2 seconds
+                // TODO: replace this with your own producer code
+                loop {
+                    tokio::time::sleep(time::Duration::from_secs(2)).await;
+                    stream_publish_message(context_stream, channel, "{\"test\":\"serialized\"}").await;
+                }
+            }
+        {{else}}
+            pub async fn producer_{{ (index . 1).unique_id }}(client: &Client, channel: &str) {
+                // This is just an example producer, publishing a message every 2 seconds
+                // TODO: replace this with your own producer code
+                loop {
+                    tokio::time::sleep(time::Duration::from_secs(2)).await;
+                    publish_message(client, channel, "{\"test\":\"serialized\"}").await;
+                }
+            }
+        {{end}}
+    {{ else }}
+        pub async fn producer_{{ (index . 1).unique_id }}(client: &Client, channel: &str) {
+            // This is just an example producer, publishing a message every 2 seconds
+            // TODO: replace this with your own producer code
+            loop {
+                tokio::time::sleep(time::Duration::from_secs(2)).await;
+                publish_message(client, channel, "{\"test\":\"serialized\"}").await;
+            }
         }
-    }
-    {{else}}
-    pub async fn producer_{{ (index . 1).unique_id }}(client: &Client, channel: &str) {
-        // This is just an example producer, publishing a message every 2 seconds
-        // TODO: replace this with your own producer code
-        loop {
-            tokio::time::sleep(time::Duration::from_secs(2)).await;
-            publish_message(client, channel, "{\"test\":\"serialized\"}").await;
-        }
-    }
-    {{end}}
-    {{end}}
+    {{ end }}
 {{ end  }}
 
 
--- templates/main.go
+++ templates/main.go
@@ -84,13 +84,14 @@ async fn main() -> Result<(), async_nats::Error> {
                 {{ if (index . 1).original_operation.bindings.nats.queue }}
                     let mut {{ (index . 1).unique_id }} = client.queue_subscribe("{{ index . 0  }}".into(), "{{ (index . 1).original_operation.bindings.nats.queue }}".into()).await?;
                 {{ else  }}
-                    let context_jetstream = jetstream::new(client);
+                    let clientcpy = client.clone();
+                    let context_jetstream = jetstream::new(clientcpy);
                     let {{ (index . 1).unique_id }} = "{{ (index . 1).original_operation.bindings.nats.streamname }}";
                     let consumer = get_consumer(&context_jetstream, &{{ (index . 1).unique_id }}).await?;
                 {{end}}
-            {{ else }}
-                let mut {{ (index . 1).unique_id }} = client.subscribe("{{ index . 0  }}".into()).await?;
-            {{end}}
+        {{ else }}
+            let mut {{ (index . 1).unique_id }} = client.subscribe("{{ index . 0  }}".into()).await?;
+        {{end}}
     {{end}}
 
     //test(&client, "foo").await;
@@ -98,26 +99,28 @@ async fn main() -> Result<(), async_nats::Error> {
     tokio::join!(
     {{ range .subscribe_channels }}
         {{ if (index . 1).original_operation.bindings }}
-             {{if (index . 1).original_operation.bindings.nats.streamname}}
-        stream_producer_{{ (index . 1).unique_id }}(&context_jetstream, "{{ (index . 1).original_operation.bindings.nats.streamname }}"),
-             {{ end }}
-        {{ else }}
-        producer_{{ (index . 1).unique_id }}(&client, "{{ index . 0  }}"),
+            {{if (index . 1).original_operation.bindings.nats.streamname}}
+                stream_producer_{{ (index . 1).unique_id }}(&context_jetstream, "{{ (index . 1).original_operation.bindings.nats.streamname }}"),
+            {{ else }}
+                producer_{{ (index . 1).unique_id }}(&client, "{{ index . 0  }}"),
             {{ end }}
+        {{ else }}
+            producer_{{ (index . 1).unique_id }}(&client, "{{ index . 0  }}"),
+        {{ end }}
     {{ end  }}
     {{ range .publish_channels  }}
         {{ if (index . 1).original_operation.bindings }}
-        {{if (index . 1).original_operation.bindings.nats.streamname}}
-        stream_listen_for_message(&consumer, stream_handler_{{ (index . 1).unique_id }}),
+            {{if (index . 1).original_operation.bindings.nats.streamname}}
+                stream_listen_for_message(&consumer, stream_handler_{{ (index . 1).unique_id }}),
+
+            {{ else }}
+                listen_for_message(&mut  {{ (index . 1).unique_id }}, handler_{{ (index . 1).unique_id }}),
+            {{ end }}
 
         {{ else }}
-        listen_for_message(&mut  {{ (index . 1).unique_id }}, handler_{{ (index . 1).unique_id }}),
+            listen_for_message(&mut  {{ (index . 1).unique_id }}, handler_{{ (index . 1).unique_id }}),
         {{ end }}
-
-        {{ else }}
-        listen_for_message(&mut  {{ (index . 1).unique_id }}, handler_{{ (index . 1).unique_id }}),
-        {{end}}
-    {{  end  }}
+    {{ end }}
     );
 
     println!("fin");
